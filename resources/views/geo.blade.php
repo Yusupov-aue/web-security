325a068489a0bdaaf040ba5ed4f75d608ca64183
wilburn <changerash@gmail.com>
2020-05-28 19:11:44 +0800

    do layouts

--- resources/views/geo.blade.php
+++ resources/views/geo.blade.php
@@ -1,44 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Wilburn</title>
-	<link rel="stylesheet" href="css/style.css">
-
-	<link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
-
-</head>
-<body>
-	<header>
-
-
-
-				<div class="logo">
-					<a class="reg" href="http://127.0.0.1:8000/reg">Регистрация</a>
-					<a class="login" href="http://127.0.0.1:8000/login">Вход</a>
-					<a href="http://127.0.0.1:8000/index"><img class="graficlogo" src="img/logo.png" alt="logo"></a>
-
-				</div>
-
-					<nav>
-
-
-						<div class="topnav" id="myTopnav">
-							<a href="http://127.0.0.1:8000/index">Главная</a>
-							<a href="http://127.0.0.1:8000/news">Новости</a>
-							<a href="http://127.0.0.1:8000/articles">Статьи</a>
-							<a href="http://127.0.0.1:8000/about">Контакты</a>
-							<a href="http://127.0.0.1:8000/question">О создателе</a>
-							<a href="http://127.0.0.1:8000/geo">Геолакация</a>
-							<a id="menu" href="#" class="icon">&#9776;</a>
-						</div>
-					</nav>
-
-
-
-
-	</header>
-
+@section('content')
+@extends('layouts.header')
 <main>
 		<div class="loc">
 			<h1><a href="#">Локация</a></h1>
@@ -48,33 +9,4 @@
 </div>
 </main>
 
-
-
-
-
-
-<footer>
-	<nav>
-		<a href="http://127.0.0.1:8000/index">Главная</a>
-				<a href="http://127.0.0.1:8000/news">Новости</a>
-				<a href="http://127.0.0.1:8000/articles">Статьи</a>
-				<a href="http://127.0.0.1:8000/about">О создателе</a>
-				<a href="http://127.0.0.1:8000/question">Поддержка</a>
-				<a href="http://127.0.0.1:8000/geo">Геолакация</a>
-	</nav>
-
-	<div class="logo">
-		<a href="http://127.0.0.1:8000/index"><img class="graficlogo" src="img/logo.png" alt="logo"></a>
-	</div>
-
-	<div class="social">
-		<a href="https://www.instagram.com/asad_wilb/"><img src="img/social icons/instagram.png" alt="instagram"></a>
-		<a href="https://vk.com/haipygraphicd"><img src="img/social icons/vk.png" alt="vk"></a>
-		<a href="https://t.me/dreamnmida"><img src="img/social icons/telegram.png" alt="telegram"></a>
-	</div>
-	<p>wilburn 2020</p>
-</footer>
-
-<script src="js/script.js"></script>
-</body>
-</html>
+@endsection('content')
